934cf0fd87300a9d06bf1ee89e7c49a9eb08ed17
mrabaneda <mrabaneda18@gmail.com>
2024-10-01 20:57:57 +0200

    fix: convert entities to classes

--- src/core/entities/admin_user.entity.ts
+++ src/core/entities/admin_user.entity.ts
@@ -8,13 +8,50 @@ import { UID } from "../value_objects/types";
 // Helpers
 // --------------------------------
 
-interface AdminUserEntity {
-  userId: UID;
-  name: string;
-  email: string;
-  photoUrl: string | null;
-  createdAt: Date;
-  updatedAt: Date;
+class AdminUserEntity {
+  constructor(
+    private readonly _userId: UID,
+    private readonly _name: string,
+    private readonly _email: string,
+    private readonly _photoUrl: string | null,
+    private readonly _createdAt: Date,
+    private readonly _updatedAt: Date
+  ) {
+    if (!this._userId || this._userId.trim().length === 0) {
+      throw new Error("AdminUserEntity: userId is required and cannot be empty.");
+    }
+    if (!this._name || this._name.trim().length === 0) {
+      throw new Error("AdminUserEntity: name is required and cannot be empty.");
+    }
+    if (!this._email || this._email.trim().length === 0) {
+      throw new Error("AdminUserEntity: email is required and cannot be empty.");
+    }
+    if (!this._createdAt) {
+      throw new Error("AdminUserEntity: created date is required.");
+    }
+    if (!this._updatedAt) {
+      throw new Error("AdminUserEntity: update date is required.");
+    }
+  }
+
+  public get userId() {
+    return this._userId;
+  }
+  public get name() {
+    return this._name;
+  }
+  public get email() {
+    return this._email;
+  }
+  public get photoUrl() {
+    return this._photoUrl;
+  }
+  public get createdAt() {
+    return this._createdAt;
+  }
+  public get updatedAt() {
+    return this._updatedAt;
+  }
 }
 
 // --------------------------------
--- src/core/entities/user.entity.ts
+++ src/core/entities/user.entity.ts
@@ -9,13 +9,53 @@ import { PreferencesTypeEnum } from "../value_objects/preferences_type_enum_valu
 // Helpers
 // -------------------------------------------------------
 
-interface UserEntity {
-  uid: UID;
-  imageId: ImageID;
-  name: string;
-  preferences: PreferencesTypeEnum;
-  createdAt: Date;
-  updatedAt: Date;
+class UserEntity {
+  constructor(
+    private readonly _userId: UID,
+    private readonly _imageId: ImageID, // TODO: revisar si mejor ruta a bucket
+    private readonly _name: string,
+    private readonly _preference: PreferencesTypeEnum,
+    private readonly _createdAt: Date,
+    private readonly _updatedAt: Date
+  ) {
+    if (!this._userId || this._userId.trim().length === 0) {
+      throw new Error("UserEntity: userId is required and cannot be empty.");
+    }
+    if (!this._imageId || this._userId.trim().length === 0) {
+      throw new Error("UserEntity: imageId is required and cannot be empty.");
+    }
+    if (!this._name || this._name.trim().length === 0) {
+      throw new Error("UserEntity: name is required and cannot be empty.");
+    }
+    if (!Object.values(PreferencesTypeEnum).includes(this._preference)) {
+      throw new Error("UserEntity: a valid preference is required.");
+    }
+    if (!this._createdAt) {
+      throw new Error("UserEntity: created date is required.");
+    }
+    if (!this._updatedAt) {
+      throw new Error("UserEntity: update date is required.");
+    }
+  }
+
+  public get userId() {
+    return this._userId;
+  }
+  public get imageId() {
+    return this._imageId;
+  }
+  public get name() {
+    return this._name;
+  }
+  public get preference() {
+    return this._preference;
+  }
+  public get createdAt() {
+    return this._createdAt;
+  }
+  public get updatedAt() {
+    return this._updatedAt;
+  }
 }
 
 // -------------------------------------------------------
